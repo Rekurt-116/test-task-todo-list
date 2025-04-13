8c2f3662609fbe836d3fcbba351d0d8e71ca0f78
Rekurt-116 <sabrlitet@gmail.com>
2025-04-13 13:23:09 +0300

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatCheckboxModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'test-task-todo-list';
+  private http = inject(HttpClient);
+  public todos: Todo[] = [];
+  public todo!: Todo;
+
+  constructor() {
+    this.http.get<Todo[]>('/todo.json').subscribe((data) => {
+      const saved = localStorage.getItem('todo-completed');
+      const savedCompleted: Record<number, boolean> = saved
+        ? JSON.parse(saved)
+        : {};
+
+      this.todos = data.map((todo) => ({
+        ...todo,
+        completed: savedCompleted[todo.id] ?? todo.completed,
+      }));
+    });
+  }
+
+  onToggleCompleted(todo: Todo) {
+    const saved = localStorage.getItem('todo-completed');
+    const savedCompleted: Record<number, boolean> = saved
+      ? JSON.parse(saved)
+      : {};
+
+    savedCompleted[todo.id] = todo.completed;
+    localStorage.setItem('todo-completed', JSON.stringify(savedCompleted));
+  }
 }
